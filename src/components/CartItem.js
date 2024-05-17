9f222cddd77f2bfc08d9cb2abe121d66ea40ff35
Mujahid002 <mujahidshaik2002@gmail.com>
2024-05-17 17:18:47 +0530

    updated based on quantity

--- src/components/CartItem.js
+++ src/components/CartItem.js
@@ -1,7 +1,18 @@
 import Image from "next/image";
+import { useGlobalContext } from "@/context/Store";
 
 export default function CartItem({ item }) {
-  const { name, emoji, quantity, price } = item;
+  const { name, emoji, quantity, price, id } = item;
+  const { cartCount, cartItems, setShouldDisplayCart, removeItemFromCart } =
+    useGlobalContext();
+
+  const handleRemoveItem = () => {
+    removeItemFromCart(id);
+    if (cartItems.length === 1 && cartItems[0].id === id) {
+      setShouldDisplayCart(false);
+    }
+    // alert(`Cart count is now ${cartCount - quantity}`);
+  };
 
   return (
     <div className="flex items-center gap-4 mb-3">
@@ -9,9 +20,12 @@ export default function CartItem({ item }) {
       <div>
         {name} <span className="text-xs">({quantity})</span>
       </div>
-      <div className="ml-auto">{price}</div>
-      <button className="hover:bg-emerald-50 transition-colors rounded-full duration-500 p-1">
-        <Image alt="delete icon" src="./trash.svg" width={20} height={20} />
+      <div className="ml-auto">{parseFloat(price) * parseFloat(quantity)}</div>
+      <button
+        onClick={handleRemoveItem}
+        className="hover:bg-emerald-50 transition-colors rounded-full duration-500 p-1"
+      >
+        <Image alt="delete icon" src="/trash.svg" width={20} height={20} />
       </button>
     </div>
   );
