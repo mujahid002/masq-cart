870fa2e9bfc868afc25d66525016544c7e9e61ca
Mujahid002 <mujahidshaik2002@gmail.com>
2024-05-17 17:18:33 +0530

    updated UI

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,10 @@
 import Product from "@/components/Product";
 import { products } from "@/data/products";
+import { useGlobalContext } from "@/context/Store";
 
 export default function Home() {
+  const { cartCount, setShouldDisplayCart, removeItemFromCart } =
+    useGlobalContext();
   return (
     <div className="grid sm:grid-cols-2 md:grid-cols-4 justify-center mx-auto gap-4 place-center flex-wrap w-100 md:max-w-[900px]">
       {products.map((product) => (
