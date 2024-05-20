fcc7a734fac5d65aa67a74787b6c9b16951fd5ae
Mujahid002 <mujahidshaik2002@gmail.com>
2024-05-20 16:10:04 +0530

    updated config file to deploy on Amoy testnet

--- backend/hardhat/hardhat.config.js
+++ backend/hardhat/hardhat.config.js
@@ -1,4 +1,42 @@
+require("@nomicfoundation/hardhat-toolbox");
+require("dotenv").config();
+require("@openzeppelin/hardhat-upgrades");
+
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
-  solidity: "0.8.24",
+  solidity: "0.8.23",
+  networks: {
+    polygonAmoy: {
+      url: process.env.AMOY_RPC_URL,
+      accounts: [process.env.PRIVATE_KEY],
+      chainId: 80002,
+      timeout: 10000,
+      confirmations: 2,
+    },
+  },
+  etherscan: {
+    apiKey: {
+      polygonAmoy: process.env.AMOY_OKLINK_API_KEY,
+    },
+    customChains: [
+      {
+        network: "polygonAmoy",
+        chainId: 80002,
+        urls: {
+          apiURL:
+            "https://www.oklink.com/api/explorer/v1/contract/verify/async/api/polygonAmoy",
+          browserURL: "https://www.oklink.com/amoy",
+        },
+      },
+    ],
+  },
+  sourcify: {
+    enabled: true,
+  },
+  settings: {
+    optimizer: {
+      enabled: true,
+      runs: 1000,
+    },
+  },
 };
