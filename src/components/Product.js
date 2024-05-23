c789d4d1e0d3e19ff71ead3f8342783f4f0403c5
Mujahid002 <mujahidshaik2002@gmail.com>
2024-05-23 13:31:07 +0530

    added discount for masq payments

--- src/components/Product.js
+++ src/components/Product.js
@@ -1,11 +1,28 @@
-import { useGlobalContext } from "@/context/Store";
+import { useGlobalContext } from "../context/Store";
 import { useState } from "react";
+import { getPrices } from "../data/products";
 
 export default function Product({ product }) {
-  const { setCartCount, setShouldDisplayCart, setCartItems, cartItems } =
-    useGlobalContext();
-  const { name, price, emoji, id } = product;
+  const {
+    maticPrice,
+    masqPrice,
+    setCartCount,
+    setShouldDisplayCart,
+    setCartItems,
+    cartItems,
+  } = useGlobalContext();
+  const { name, price, emoji, id, discount } = product;
   const [quantity, setQuantity] = useState(1);
+  const [ratio, setRatio] = useState(1);
+
+  const getRatio = async () => {
+    try {
+      const { maticPrice, masqPrice } = await getPrices();
+      console.log(maticPrice, masqPrice);
+    } catch (error) {
+      console.error("Unable to call getRatio", error);
+    }
+  };
 
   const addItemToCart = () => {
     const newItem = {
@@ -14,6 +31,7 @@ export default function Product({ product }) {
       price,
       id,
       quantity,
+      discount,
     };
 
     setCartItems((prevItems) => {
@@ -49,9 +67,26 @@ export default function Product({ product }) {
 
   return (
     <article className="flex flex-col gap-3 bg-white p-8 rounded-xl shadow-md text-center mb-6">
-      <div className="text-8xl cursor-default">{emoji}</div>
+      <div className="text-7xl cursor-default">{emoji}</div>
       <div className="text-lg">{name}</div>
-      <div className="text-2xl font-semibold mt-auto">{price}</div>
+      <div className="text-xl font-semibold mt-auto">$ {price}</div>
+      <div className="text-sm font-semibold mt-auto">
+        {(price / maticPrice).toFixed(4)} Matic
+      </div>
+      <div className="text-sm font-semibold mt-auto">
+        {((price * (100 - parseFloat(discount))) / 100 / masqPrice).toFixed(4)}{" "}
+        Masq{" "}
+        {/* <span className="text-sm font-semibold mt-auto text-green-300">
+          ~{discount}%
+        </span> */}
+      </div>
+      <div className="text-sm font-semibold mt-auto text-green-300">
+        ~{discount}% OFF
+      </div>
+
+      {/* <div className="text-sm font-semibold mt-auto text-green-300">
+        {discount} Discount
+      </div> */}
       <div className="flex justify-around items-center mt-4 mb-2">
         <button
           onClick={decreaseQuantity}
@@ -71,7 +106,7 @@ export default function Product({ product }) {
         onClick={() => {
           addItemToCart();
           setShouldDisplayCart(true);
-          // setQuantity(1);
+          // getRatio();
         }}
         className="bg-emerald-50 hover:bg-emerald-500 hover:text-white transition-colors duration-500 text-emerald-500 rounded-md px-5 py-2"
       >
