f10f69eb4209f64f70a38cc6f47858fd0580030a
Mujahid002 <mujahidshaik2002@gmail.com>
2024-05-23 13:30:03 +0530

    updated pricing

--- src/components/CartItem.js
+++ src/components/CartItem.js
@@ -1,5 +1,6 @@
 import Image from "next/image";
-import { useGlobalContext } from "@/context/Store";
+import { useGlobalContext } from "../context/Store";
+import { useState } from "react";
 
 export default function CartItem({ item }) {
   const { name, emoji, quantity, price, id } = item;
@@ -21,7 +22,9 @@ export default function CartItem({ item }) {
       <div>
         {name} <span className="text-xs">({quantity})</span>
       </div>
-      <div className="ml-auto">{parseFloat(price) * parseFloat(quantity)}</div>
+      <div className="ml-auto">
+        {(parseFloat(price) * parseFloat(quantity)).toFixed(4)}
+      </div>
       <button
         onClick={handleRemoveItem}
         className="hover:bg-emerald-50 transition-colors rounded-full duration-500 p-1"
