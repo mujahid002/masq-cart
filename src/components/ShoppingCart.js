1adca8826f91d2277c899578b07473ce1dc2bfb1
Mujahid002 <mujahidshaik2002@gmail.com>
2024-05-23 13:31:45 +0530

    shows total checkout costs for both native & tokens

--- src/components/ShoppingCart.js
+++ src/components/ShoppingCart.js
@@ -1,12 +1,38 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import CartItem from "./CartItem";
-import CheckoutButton from "./CheckoutButton";
-import { useGlobalContext } from "@/context/Store";
-
+import MaticCheckOut from "./MaticCheckOut";
+import MasqCheckOut from "./MasqCheckOut";
+import { useGlobalContext } from "../context/Store";
 
 export default function ShoppingCart() {
-  const { shouldDisplayCart, setShouldDisplayCart, cartCount, cartItems } =
+  const { shouldDisplayCart, maticPrice, masqPrice, cartCount, cartItems } =
     useGlobalContext();
+
+  const [maticAmount, setMaticAmount] = useState(0);
+  const [masqAmount, setMasqAmount] = useState(0);
+
+  useEffect(() => {
+    // Calculate Matic amount
+    let maticTotal = 0;
+    cartItems.forEach((item) => {
+      const { quantity, price } = item;
+      const totalMatic = (price * quantity) / maticPrice;
+      maticTotal += totalMatic;
+    });
+    setMaticAmount(maticTotal);
+
+    // Calculate Masq amount
+    let masqTotal = 0;
+    cartItems.forEach((item) => {
+      const { quantity, price, discount } = item;
+      const discountedPrice = (price * (100 - discount)) / 100;
+      console.log(discount);
+      const totalMasq = (discountedPrice * quantity) / masqPrice; // Calculate total in Masq for this item
+      masqTotal += totalMasq; // Accumulate total Masq amount
+    });
+    setMasqAmount(masqTotal); // Update state with total Masq amount
+  }, [cartItems, maticPrice, masqPrice]);
+
   return (
     <div
       className={`bg-white flex flex-col absolute right-3 md:right-9 top-14 w-80 py-4 px-4 shadow-[0_5px_15px_0_rgba(0,0,0,.15)] rounded-md transition-opacity duration-500 ${
@@ -18,8 +44,16 @@ export default function ShoppingCart() {
           {Object.values(cartItems ?? {}).map((entry) => (
             <CartItem key={entry.id} item={entry} />
           ))}
-
-          <CheckoutButton />
+          <MaticCheckOut />
+          <MasqCheckOut />
+          <p>
+            Total in Matic: {maticAmount.toFixed(4)} (~${" "}
+            {(maticAmount * maticPrice).toFixed(2)})
+          </p>
+          <p>
+            Total in Masq: {masqAmount.toFixed(4)} (~${" "}
+            {(masqAmount * masqPrice).toFixed(2)})
+          </p>
         </>
       ) : (
         <div className="p-5">You have no items in your cart</div>
