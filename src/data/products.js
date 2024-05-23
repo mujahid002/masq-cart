c3f32c4fbe5a2db74ecc6fa013f4676e25c35688
Mujahid002 <mujahidshaik2002@gmail.com>
2024-05-23 13:33:20 +0530

    fetch matic & masq prices in USD

--- src/data/products.js
+++ src/data/products.js
@@ -1,30 +1,71 @@
+// src/data/products.js
+
+import Moralis from "moralis";
+
+export async function getPrices() {
+  try {
+    // Fetch MATIC price
+    const maticDataResponse = await fetch(
+      `https://api.polygonscan.com/api?module=stats&action=maticprice&apikey=${process.env.NEXT_PUBLIC_POLYGON_API_KEY}`
+    );
+    if (!maticDataResponse.ok) {
+      throw new Error("Failed to fetch MATIC price");
+    }
+    const maticData = await maticDataResponse.json();
+    const maticPrice = parseFloat(maticData.result.maticusd);
+    if (isNaN(maticPrice)) {
+      throw new Error("Invalid MATIC price data");
+    }
+
+    // Fetch MASQ price
+    const response = await Moralis.EvmApi.token.getTokenPrice({
+      chain: "0x89",
+      address: "0xEe9A352F6aAc4aF1A5B9f467F6a93E0ffBe9Dd35",
+    });
+
+    const masqPrice = parseFloat(response.raw.usdPrice);
+    if (isNaN(masqPrice)) {
+      throw new Error("Invalid MASQ price data");
+    }
+
+    return {
+      maticPrice: maticPrice.toFixed(4),
+      masqPrice: masqPrice.toFixed(4),
+    };
+  } catch (error) {
+    console.error("Error fetching prices:", error);
+    return { error: error.message };
+  }
+}
+
 export const products = [
   {
     id: 1,
-    name: "Mobile",
-    price: "100",
-    emoji: "📱",
-    currency: "Doller",
+    name: "Banana",
+    price: "0.1",
+    emoji: "🍌",
+    discount: "10",
   },
   {
     id: 2,
-    name: "Laptop",
-    price: "200",
-    emoji: "💻",
-    currency: "Doller",
+    name: "Apple",
+    price: "0.4",
+    emoji: "🍎",
+    discount: "20",
   },
   {
     id: 3,
-    name: "Bi-Cycle",
-    price: "120",
-    emoji: "🚲",
-    currency: "Doller",
+    name: "Watermelon",
+    price: "0.5",
+    emoji: "🍉",
+    discount: "20",
   },
+
   {
     id: 4,
-    name: "Camera",
-    price: "50",
-    emoji: "📸",
-    currency: "Doller",
+    name: "Pineapple",
+    price: "0.6",
+    emoji: "🍍",
+    discount: "30",
   },
 ];
