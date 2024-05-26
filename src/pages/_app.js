19afbe9b30700a7d4baa0390da1f3ea339cd98e8
Mujahid002 <mujahidshaik2002@gmail.com>
2024-05-27 02:23:10 +0530

    updated

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,5 @@
-import Layout from "@/components/Layout";
-import "@/styles/globals.css";
+import Layout from "../components/Layout";
+import "../styles/globals.css";
 import { GlobalContextProvider } from "../context/Store";
 import { MoralisProvider } from "../context/MoralisProvider";
 
